1c820464daa67e1783ef3df28a6fc04e868fc380
Antonio José Leal Ferrández <ajleal@atsistemas.com>
2024-07-06 18:00:35 +0200

    Añadir feed RSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://unique-sprinkles-2c1ef0.netlify.app/"
+});
