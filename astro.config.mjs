7933efba7432f0a0bdce06137379f421e12c7aa9
Antonio José Leal Ferrández <ajleal@atsistemas.com>
2024-07-06 18:12:55 +0200

    Añade preact con botón hidratado para mostrar interacción con cliente

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://unique-sprinkles-2c1ef0.netlify.app/"
+  site: "https://unique-sprinkles-2c1ef0.netlify.app/",
+  integrations: [preact()]
 });
